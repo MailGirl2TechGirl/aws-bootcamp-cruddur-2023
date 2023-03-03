dbe8a1402b99647ee41377d0e975d4dae3fdd5e1
MailGirl2TechGirl <brookeleighraines+cloud@gmail.com>
2023-03-03 17:23:08 +0000

    Commenting out watch for spend considerations

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,13 +33,13 @@ import logging
 from time import strftime
 
 # Configuring Logger to Use CloudWatch
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group = "cruddur")
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
-LOGGER.info("test log")
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler('Cruddur')
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
+#LOGGER.info("test log")
 
 
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -119,7 +119,7 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @cross_origin()
 def data_home():
-  data = HomeActivities.run(Logger = LOGGER)
+  data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
